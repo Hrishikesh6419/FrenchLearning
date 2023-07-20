c8a8d551ab5bb2776848afd6dfd8aebfeba92c7b
d120215 <hrishikeshheera.darshan@citizensbank.com>
2023-07-20 12:29:19 -0400

    Added pronunciation

--- app/src/main/java/com/example/frenchlearning/word_detail/view/WordDetailFragment.kt
+++ app/src/main/java/com/example/frenchlearning/word_detail/view/WordDetailFragment.kt
@@ -1,6 +1,7 @@
 package com.example.frenchlearning.word_detail.view
 
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,10 +12,13 @@ import com.example.frenchlearning.R
 import com.example.frenchlearning.databinding.FragmentWordDetailBinding
 import com.example.frenchlearning.util.CommonUtil
 import com.example.frenchlearning.word_detail.WordDetailViewModel
+import java.util.*
 
 class WordDetailFragment : Fragment() {
 
     private var _binding: FragmentWordDetailBinding? = null
+    private var textToSpeech: TextToSpeech? = null
+
     private val binding get() = _binding!!
     private val viewModel: WordDetailViewModel by viewModels()
 
@@ -28,6 +32,26 @@ class WordDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        observeWord()
+        setHeadingPronunciationListener()
+    }
+
+    private fun setHeadingPronunciationListener() {
+        textToSpeech = TextToSpeech(requireContext()) { status ->
+            if (status != TextToSpeech.ERROR) {
+                textToSpeech?.language = Locale.FRENCH
+            }
+        }
+
+        binding.layoutPronunciation.setOnClickListener {
+            val word = viewModel.word.value
+            if (word != null) {
+                textToSpeech?.speak(word.frenchWord, TextToSpeech.QUEUE_FLUSH, null, "")
+            }
+        }
+    }
+
+    private fun observeWord() {
         viewModel.word.observe(viewLifecycleOwner) { word ->
             binding.englishWord.setCustomText(word.englishWord)
             binding.frenchWord.setCustomText(word.frenchWord)
@@ -49,6 +73,9 @@ class WordDetailFragment : Fragment() {
 
     override fun onDestroyView() {
         super.onDestroyView()
+        textToSpeech?.stop()
+        textToSpeech?.shutdown()
+        textToSpeech = null
         _binding = null
     }
 }
